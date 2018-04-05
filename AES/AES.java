d0171cd0e6a168bf9aed73ee6a2fb9c0a50efc90
PirateJack <35343459+pateljay269@users.noreply.github.com>
2018-04-05 18:16:26 +0530

    Updated
    Updated

--- AES/AES.java
+++ AES/AES.java
@@ -1,50 +1,143 @@
+
 /*
  * Project - DES
  * Created By Patel Jay.
  * On 19 3 2018.
  */
 
+import java.util.Scanner;
+
 public class AES {
+
+    private static final int ROUND = 10;
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        String input;
-        char c;
+        String input, ky;
 
         System.out.print("Enter Plain Text: ");
         input = in.nextLine();
-        System.out.print("Enter Iv(Any 1 Character): ");
-        c = in.next().charAt(0);
+        System.out.print("Enter Iv: ");
+        ky = in.nextLine();
 
-        String key = Cons.char2Bin(c);
+//        input = "1234567812345678";
+//        ky = "123456";
+        String[][] key = keyGen(ky);
 
-        setData(input, key);
+        String c1 = encrypt(input, key);
+
+        System.out.println("Cipher Text:" + c1);
+
     }
 
-    private static void setData(String input, String ky) {
-        int tky[] = new int[128];
-        System.arraycopy(Const.lfsr(ky), 0, tky, 0, 128);
-        String key = "";
+    private static String encrypt(String input, String[][] key) {
+        String output = "";
 
-        for (int h = 0; h < 128; h += 8) {
-            String str = "";
-            for (int i = 0; i < 8; i++) {
-                str += tky[h + i];
+        for (int h = 0; h < input.length(); h += 16) {
+            String s1;
+            try {
+                s1 = input.substring(h, h + 16);
+            } catch (Exception e) {
+                s1 = input.substring(h, input.length());
             }
-            key += (char) Integer.parseInt(str, 2);
+
+            String Ai[] = new String[16];
+            for (int i = 0; i < 16; i++) {
+                Ai[i] = "00";
+            }
+
+            for (int j = 0; j < s1.length(); j++) {
+                Ai[j] = Cons.char2Hex(s1.charAt(j));
+            }
+
+            //Key Addition Layer-Key0
+            for (int i = 0; i < 16; i++) {
+                Ai[i] = Cons.hexXOR(Ai[i], key[0][i]);
+            }
+
+            for (int g = 0; g < ROUND; g++) {
+
+                if (g == ROUND - 1) {
+                    break;
+                }
+
+                Ai = Cons.permutation(Ai, Cons.SUB);
+                String Bi[][] = Cons.leftshiftRow(Ai);  //Shift Row
+                Bi = Cons.mixCol(Bi);                   //Mix Column
+                Ai = Cons.merge(Cons.colRow(Bi));
+
+                String keyR[] = key[g + 1];
+
+                for (int i = 0; i < Ai.length; i++) {
+                    Ai[i] = Cons.hexXOR(Ai[i], keyR[i]);
+                }
+            }
+
+            //Last Round
+            Ai = Cons.permutation(Ai, Cons.SUB);
+            Ai = Cons.merge(Cons.leftshiftRow(Ai));
+            Ai = Cons.merge(Cons.colRow(Cons.half4(Ai)));
+
+            for (int i = 0; i < Ai.length; i++) {
+                Ai[i] = Cons.hexXOR(Ai[i], key[key.length - 1][i]);
+            }
+
+            output += Cons.hex2Char(Ai);
         }
 
-        System.out.print(key);
-
-        boolean isEnc = true;
-        String te = encDec(input, key, isEnc);
-    }
-
-    private static String encDec(String input, String ky, boolean isEnc) {
-        String output = "";
-        
-        
         return output;
     }
+
+    private static String[][] keyGen(String sKey) {
+
+        int len = sKey.length();
+        if (len <= 16) {
+            int k = 16 - len;
+            for (int i = 0; i < k; i++) {
+                sKey += "p";
+            }
+        }
+
+        String key[][] = new String[ROUND + 1][16];
+        for (int j = 0; j < 16; j++) {
+            key[0][j] = Cons.char2Hex(sKey.charAt(j));
+        }
+
+        String ky[][] = Cons.half4(key[0]);
+
+        for (int g = 1; g < key.length; g++) {
+            String pKey[][] = ky;
+            ky = Cons.rotWord(ky);
+            ky[0] = Cons.permutation(ky[0], Cons.SUB);
+
+            for (int i = 0; i < ky[0].length; i++) {
+                String t2 = pKey[0][i], t1 = ky[0][i], temp = "";
+                for (int j = 0; j < t1.length(); j++) {
+                    String t3 = Cons.hexXOR(t1.charAt(j) + "", t2.charAt(j) + "");
+                    if (i == 0) {
+                        String tt = Cons.RCON[g - 1].charAt(j) + "";
+                        temp += Cons.hexXOR(tt, t3);
+                    } else {
+                        temp += t3;
+                    }
+                }
+                ky[0][i] = temp;
+            }
+
+            for (int i = 1; i < ky.length; i++) {
+                for (int j = 0; j < ky[i].length; j++) {
+                    String t1 = pKey[i][j], t2 = ky[i - 1][j], temp = "";
+                    for (int k = 0; k < t1.length(); k++) {
+                        temp += Cons.hexXOR(t1.charAt(k) + "", t2.charAt(k) + "");
+                    }
+                    ky[i][j] = temp;
+                }
+            }
+            key[g] = Cons.merge(ky);
+        }
+
+        return key;
+    }
+
 }
--- AES/Cons.java
+++ AES/Cons.java
@@ -1,7 +1,8 @@
+
 /*
  * Project - DES
  * Created By Patel Jay.
- * On 19 3 2018.
+ * On 21 3 2018.
  */
 
 import java.math.BigInteger;
@@ -10,44 +11,77 @@ public class Cons {
 
     public static final String[] RCON = {"01", "02", "04", "08", "10", "20", "40", "80", "1b", "36"};
 
-    public static final int[][] MixColInt = {
-            {02, 03, 01, 01},
-            {01, 02, 03, 01},
-            {01, 01, 02, 03},
-            {03, 01, 01, 02}
+    public static final String[][] SUB = {
+            {"63", "7C", "77", "7B", "F2", "6B", "6F", "C5", "30", "01", "67", "2B", "FE", "D7", "AB", "76"}, //0
+            {"CA", "82", "C9", "7D", "FA", "59", "47", "F0", "AD", "D4", "A2", "AF", "9C", "A4", "72", "C0"}, //1
+            {"B7", "FD", "93", "26", "36", "3F", "F7", "CC", "34", "A5", "E5", "F1", "71", "D8", "31", "15"}, //2
+            {"04", "C7", "23", "C3", "18", "96", "05", "9A", "07", "12", "80", "E2", "EB", "27", "B2", "75"}, //3
+            {"09", "83", "2C", "1A", "1B", "6E", "5A", "A0", "52", "3B", "D6", "B3", "29", "E3", "2F", "84"}, //4
+            {"53", "D1", "00", "ED", "20", "FC", "B1", "5B", "6A", "CB", "BE", "39", "4A", "4C", "58", "CF"}, //5
+            {"D0", "EF", "AA", "FB", "43", "4D", "33", "85", "45", "F9", "02", "7F", "50", "3C", "9F", "A8"}, //6
+            {"51", "A3", "40", "8F", "92", "9D", "38", "F5", "BC", "B6", "DA", "21", "10", "FF", "F3", "D2"}, //7
+            {"CD", "0C", "13", "EC", "5F", "97", "44", "17", "C4", "A7", "7E", "3D", "64", "5D", "19", "73"}, //8
+            {"60", "81", "4F", "DC", "22", "2A", "90", "88", "46", "EE", "B8", "14", "DE", "5E", "0B", "DB"}, //9
+            {"E0", "32", "3A", "0A", "49", "06", "24", "5C", "C2", "D3", "AC", "62", "91", "95", "E4", "79"}, //a
+            {"E7", "C8", "37", "6D", "8D", "D5", "4E", "A9", "6C", "56", "F4", "EA", "65", "7A", "AE", "08"}, //b
+            {"BA", "78", "25", "2E", "1C", "A6", "B4", "C6", "E8", "DD", "74", "1F", "4B", "BD", "8B", "8A"}, //c
+            {"70", "3E", "B5", "66", "48", "03", "F6", "0E", "61", "35", "57", "B9", "86", "C1", "1D", "9E"}, //d
+            {"E1", "F8", "98", "11", "69", "D9", "8E", "94", "9B", "1E", "87", "E9", "CE", "55", "28", "DF"}, //e
+            {"8C", "A1", "89", "0D", "BF", "E6", "42", "68", "41", "99", "2D", "0F", "B0", "54", "BB", "16"} //f
     };
 
-    public static final String[][] MixColStr = {
-            {"02", "03", "01", "01"},
-            {"01", "02", "03", "01"},
-            {"01", "01", "02", "03"},
-            {"03", "01", "01", "02"}
+    public static final int[][] MixCol = {
+            {2, 3, 1, 1},
+            {1, 2, 3, 1},
+            {1, 1, 2, 3},
+            {3, 1, 1, 2}
     };
 
-    public static void prAr(String ar[]) {
-        System.out.print("");
-        for (String a : ar) {
-            System.out.print(a + " ");
+    public static String[][] rotWord(String[][] key) {
+        String arr[][] = new String[4][4];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                arr[i][j] = "";
+            }
         }
-        System.out.println("");
+
+        int n = key[3].length;
+        String temp = key[3][0];
+        System.arraycopy(key[3], 1, arr[3], 0, n - 1);
+        arr[3][n - 1] = temp;
+
+        String t[] = arr[0];
+        arr[0] = arr[3];
+        arr[3] = t;
+
+        return arr;
     }
 
-    public static String char2Bin(int ascii) {
-        int ar[] = setBit8(ascii);
-        String str = "";
-        for (int a : ar) {
-            str += a;
+    public static String hexXOR(String s1, String s2) {
+        String xor = "";
+        if (s1.length() == s2.length()) {
+            for (int i = 0; i < s1.length(); i++) {
+                int n1 = Integer.parseInt(s1.charAt(i) + "", 16);
+                int n2 = Integer.parseInt(s2.charAt(i) + "", 16);
+                int n3 = n1 ^ n2;
+                xor += String.format("%X", n3);
+            }
         }
-        return str;
+        return xor;
     }
 
     public static String char2Hex(int ascii) {
         int ar[] = setBit8(ascii);
         String str = "";
-        for (int a : ar) {
-            str += a;
+
+        for (int i = 0; i < ar.length; i += 4) {
+            String c = "";
+            for (int j = 0; j < 4; j++) {
+                c += ar[i + j];
+            }
+            str += bin2Hex(c);
         }
-        return bin2Hex(str);
+        return str;
     }
 
     private static String bin2Hex(String binary) {
@@ -55,8 +89,18 @@ public class Cons {
         return Integer.toString(dec, 16).toUpperCase();
     }
 
-    private static String hex2Bin(String hex) {
+    public static String hex2Bin(String hex) {
         return new BigInteger(hex, 16).toString(2);
+    }
+
+    private static int[] hex2BinAr(String hex) {
+        String b = new BigInteger(hex, 16).toString(2);
+        int size = 4;
+        int plain[] = new int[size];
+        for (int k = size, j = b.length() - 1; j >= 0; j--) {
+            plain[--k] = Integer.parseInt(b.charAt(j) + "");
+        }
+        return plain;
     }
 
     public static String[] merge(String arr[][]) {
@@ -70,14 +114,20 @@ public class Cons {
         return ar;
     }
 
-    public static String[] leftShift(String[] arr, int round) {
-        for (int shift = 0; shift < round; shift++) {
-            int n = arr.length;
-            String temp = arr[0];
-            System.arraycopy(arr, 1, arr, 0, n - 1);
-            arr[n - 1] = temp;
+    public static String[] permutation(String ar[], String table[][]) {
+        try {
+            String arr[] = new String[ar.length];
+            for (int i = 0; i < arr.length; i++) {
+                String xy = ar[i];
+                int x = Integer.parseInt(hex2Bin(xy.charAt(0) + ""), 2);
+                int y = Integer.parseInt(hex2Bin(xy.charAt(1) + ""), 2);
+                arr[i] = table[x][y];
+            }
+            return arr;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
-        return arr;
+        return null;
     }
 
     public static String[][] half4(String arr[]) {
@@ -90,38 +140,106 @@ public class Cons {
         return ar;
     }
 
-    public static int[] lfsr(String iv) {
-        try {
-            int size = (int) (Math.pow(2, iv.length()) - 1);
-            int[] queue = new int[size];
-            Double.parseDouble(iv);
+    public static String[][] mixCol(String arB[][]) {
+        String mixCol[][] = new String[4][4];
 
-            for (int i = 0; i < iv.length(); i++) {
-                int t = Integer.parseInt(iv.charAt(i) + "");
-                if (t < 0 || t > 1) {
-                    throw new NumberFormatException("Enter Only 0 And 1");
+        arB = colRow(arB);
+        for (int r = 0; r < mixCol.length; r++) {
+
+            String col[] = new String[4];
+            for (int j = 0; j < 4; j++) {
+                String temp[] = new String[4];
+                for (int c = 0; c < 4; c++) {
+
+                    switch (MixCol[j][c]) {
+                        case 2:
+                            temp[c] = dot2(arB[r][c]);
+                            break;
+
+                        case 3:
+                            String old = arB[r][c];
+                            temp[c] = hexXOR(dot2(arB[r][c]), old);
+                            break;
+
+                        default:
+                            temp[c] = arB[r][c];
+                            break;
+                    }
                 }
-            }
 
-            int j = 0;
-            for (int i = iv.length() - 1; i >= 0; i--) {
-                queue[j++] = Integer.parseInt(iv.charAt(i) + "");
+                String xor = hexXOR(hexXOR(temp[0], temp[1]), hexXOR(temp[2], temp[3]));
+                col[j] = xor;
             }
-
-            for (int i = iv.length(); i < size; i++) {
-                int t = (queue[i - iv.length()] + queue[i - iv.length() + 1]) % 2;
-                queue[j++] = t;
-            }
-
-            return queue;
-        } catch (NumberFormatException e) {
-            e.printStackTrace();
-            System.out.println(e.getMessage());
+            mixCol[r] = col;
         }
-        return null;
+        mixCol = colRow(mixCol);
+        return mixCol;
     }
 
-    public static int[] setBit8(int ascii) {
+    public static String dot2(String hex) {
+        final int[] MIXOR = {0, 0, 0, 1, 1, 0, 1, 1};
+
+        int ar[] = new int[8];
+        for (int j = 0, k = 0; j < hex.length(); j++) {
+            int pBit[] = hex2BinAr(hex.charAt(j) + "");
+            for (int aBit : pBit) {
+                ar[k++] = aBit;
+            }
+        }
+
+        boolean isXor = ar[0] == 1;
+        ar = leftShift(ar, 1);
+        ar[ar.length - 1] = 0;
+
+        if (isXor) {
+            for (int j = 0; j < ar.length; j++) {
+                ar[j] = ar[j] ^ MIXOR[j];
+            }
+        }
+
+        String Ohex = "";
+        for (int aBit : ar) {
+            Ohex += aBit;
+        }
+
+        Ohex = char2Hex((char) Integer.parseInt(Ohex, 2));
+
+        return Ohex;
+    }
+
+    private static int[] leftShift(int arr[], int round) {
+        for (int shift = 0; shift < round; shift++) {
+            int n = arr.length;
+            int temp = arr[0];
+            System.arraycopy(arr, 1, arr, 0, n - 1);
+            arr[n - 1] = temp;
+        }
+        return arr;
+    }
+
+    public static String[][] leftshiftRow(String[] ar) {
+        String arr[][] = half4(ar);
+
+        arr = colRow(arr);
+        arr[0] = leftShift(arr[0], 0);
+        arr[1] = leftShift(arr[1], 1);
+        arr[2] = leftShift(arr[2], 2);
+        arr[3] = leftShift(arr[3], 3);
+
+        return arr;
+    }
+
+    private static String[] leftShift(String[] arr, int round) {
+        for (int shift = 0; shift < round; shift++) {
+            int n = arr.length;
+            String temp = arr[0];
+            System.arraycopy(arr, 1, arr, 0, n - 1);
+            arr[n - 1] = temp;
+        }
+        return arr;
+    }
+
+    private static int[] setBit8(int ascii) {
         int size = 8;
         int plain[] = new int[size];
         String pt = Integer.toBinaryString(ascii);
@@ -131,4 +249,28 @@ public class Cons {
         return plain;
     }
 
+    public static String hex2Char(String[] ar) {
+        String ct = "";
+
+        for (int i = 0; i < ar.length; i++) {
+            String bin = "";
+            for (int j = 0; j < ar[i].length(); j++) {
+                bin += hex2Bin(ar[i].charAt(j) + "");
+            }
+            ct += (char) Integer.parseInt(bin, 2);
+        }
+
+        return ct;
+    }
+
+    //Column To Row <=> Row To Column Transform
+    public static String[][] colRow(String[][] ar) {
+        String arr[][] = new String[4][4];
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                arr[j][i] = ar[i][j];
+            }
+        }
+        return arr;
+    }
 }
